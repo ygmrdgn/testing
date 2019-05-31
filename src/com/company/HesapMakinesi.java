ef94b212c37db9092bd0dba42c890635c9ced4b3
yağmur <ygmrdgn0123@gmail.com>
2019-05-31 12:53:20 +0300

    first commit

---
+++ src/com/company/HesapMakinesi.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HesapMakinesi {
+
+    public int Topla(int deger1, int deger2){
+
+        return deger1+deger2;
+    }
+    public int Carp(int deger1, int deger2){
+
+        return deger1*deger2;
+    }
+}
